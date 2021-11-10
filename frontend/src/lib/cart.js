0eaa027cf8bd65c6022cbbecb0eafe1549a42cc7
Abdi Arr <abdirizaqarr@gmail.com>
2021-11-10 13:38:48 +0000

    made cart page and crud cart functionallity

--- frontend/src/lib/cart.js
+++ frontend/src/lib/cart.js
@@ -14,10 +14,33 @@ export const makeAddToCartReq = async (userId, item) => {
   }
 };
 
-/* needs work */
-export const makeIncQuantReq = async (userId, itemId) => {
-  const userCartReq = await getUsersCart(userId);
-  const userCart = userCartReq.data;
-
-  console.log(userCart);
+export const getCartDetails = async (cartItems) => {
+  const allItemsReq = await axios.get('/items');
+  const items = allItemsReq.data;
+  const cartDetails = items.map((item) => {
+    const match = cartItems.find((cartItem) => cartItem.id === item.id);
+    if (match) {
+      return {
+        ...match,
+        ...item,
+      };
+    }
+  });
+  return cartDetails.filter((item) => item);
 };
+
+export const updateCartItemQuant = async (itemId, quantity) => {
+  await axios.put(`/cartItems/${itemId}`, { quantity: quantity });
+};
+
+export const deleteItemFromCart = async (itemId) => {
+  await axios.delete(`/cartItems/${itemId}`);
+};
+
+/* needs work */
+// export const makeIncQuantReq = async (userId, itemId) => {
+//   const userCartReq = await getUsersCart(userId);
+//   const userCart = userCartReq.data;
+
+//   console.log(userCart);
+// };
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,7 @@
 export { default as Categories } from './Categories';
 export { default as Home } from './Home';
 export { default as Category } from './Category';
+export { default as Cart } from './Cart';
 // export { default as Restaurant } from "./Restaurant";
 export { Login } from './Login';
 export { Signup } from './Signup';
