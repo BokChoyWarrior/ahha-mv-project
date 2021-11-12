563ae7156748d8058c7e17f7beb9b13f92e8bafb
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-11-12 12:29:35 +0000

    Cleanup category page and daniel miller

--- backend/seedDB.js
+++ backend/seedDB.js
@@ -16,19 +16,19 @@ const { sequelize, CartItem, Cart, Item, Category } = require('./database/models
 
 //   const cat2 = await Category.create({
 //     name: 'Jewellery',
-//     imageLink: `https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260`,
+//     imageLink: `https://c.pxhere.com/photos/94/10/church_rosary_religion_christian_catholic_faith-1029472.jpg!s`,
 //   });
 //   const cat3 = await Category.create({
 //     name: 'Electronics',
-//     imageLink: `https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260`,
+//     imageLink: `https://p0.pikist.com/photos/786/200/macro-electronics-printed-circuit-boards-integrated-circuits-circuit-board-computer-engineering-chip-thumbnail.jpg`,
 //   });
 //   const cat4 = await Category.create({
 //     name: "Women's Clothing",
-//     imageLink: `https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260`,
+//     imageLink: `https://c.pxhere.com/photos/cb/95/fashion_girl_glamour_attractive_woman_model_clothing_traditional-797484.jpg!s`,
 //   });
 //   const cat1 = await Category.create({
 //     name: "Men's Clothing",
-//     imageLink: `https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260`,
+//     imageLink: `https://ca.slack-edge.com/TCX5TJ8DD-U01CRLBTTR6-c43403faeb3f-512`,
 //   });
 
 //   items.forEach(async (_item) => {
